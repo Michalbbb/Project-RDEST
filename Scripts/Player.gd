17a596193c7ff84478842942bf7a5611ad201ed6
WojciechBrocolli <Wojciech.brancewicz@gmail.com>
2023-11-11 17:03:35 +0100

    restart player position

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,7 +13,8 @@ var gravity = 9.8 #ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var player = $"."
 #@onready var surface = $"../CSGBox3D"						
 @onready var anim_tree = $CollisionShape3D/PhMainHero2/AnimationTree
-
+#ground_level 
+var ground_level = [0,0]
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -33,10 +34,16 @@ func _physics_process(delta):
 	# Quitting game
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	# Reset character position
+	if player.position.y <= ground_level[1]:
+		player.position.x = 0
+		player.position.y = 0
+		player.position.z = 0
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

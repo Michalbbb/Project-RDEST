af044a43aef7a242dce98729f350c75df327c1cb
Przemysław Baniewicz <92544043+Przemo532@users.noreply.github.com>
2023-12-10 15:08:14 +0100

    Interaction mechanic
    Interactor and interactable nodes added for future use with any interactable entity.

---
+++ Systems/Interaction/Interactable.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+class_name Interactable
+
+# Emitted when an Interactor starts looking at me.
+signal focused(interactor: Interactor)
+# Emitted when an Interactor stops looking at me.
+signal unfocused(interactor: Interactor)
+# Emitted when an Interactor interacts with me.
+signal interacted(interactor: Interactor)
+
+
+func _on_focused(interactor):
+	pass # Replace with function body.
+
+
+func _on_interacted(interactor):
+	pass # Replace with function body.
+
+
+func _on_unfocused(interactor):
+	pass # Replace with function body.

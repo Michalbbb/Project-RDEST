4700d134dcc38e6d26be595715731dce928ddd0c
Przemysław Baniewicz <92544043+Przemo532@users.noreply.github.com>
2023-12-10 15:57:22 +0100

    Added a popup to sign node.
    Popup is still empty but it shows up when interacting with object and is closes when player moves away far enough from the "sign" or when player decides to close it manually.

--- Props/Sign/Sign.gd
+++ Props/Sign/Sign.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var popup = $FastTravelWindow
+var is_popup_open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,15 +12,22 @@ func _ready():
 func _process(delta):
 	pass
 
-
+# Called when object enters interactable distance
 func _on_interactable_focused(interactor):
-	pass # Replace with function body.
+	pass
 
-
+# Called when object is interacted with
 func _on_interactable_interacted(interactor):
-	print("e")
-	pass # Replace with function body.
+	if not is_popup_open:
+		popup.show()
+		is_popup_open = true
 
-
+# Called when object exits interactable distance
 func _on_interactable_unfocused(interactor):
-	pass # Replace with function body.
+	popup.hide()
+	is_popup_open = false
+
+# Handling closing of the window
+func _on_fast_travel_window_close_requested():
+	popup.hide()
+	is_popup_open = false

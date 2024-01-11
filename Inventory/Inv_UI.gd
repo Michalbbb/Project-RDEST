898caa6e6ec28c5b5275816bf6e64a543c3d32ab
Przemysław Baniewicz <92544043+Przemo532@users.noreply.github.com>
2024-01-11 15:56:46 +0100

    Fixing inventory

--- Inventory/Inv_UI.gd
+++ Inventory/Inv_UI.gd
@@ -1,1 +1,28 @@
 extends Control
+
+@onready var inv: Inv = preload("res://Inventory/PlayerInv.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
+var is_open = false
+
+func _ready():
+	update_slots()
+	close()
+
+func update_slots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
+
+func _process(delta):
+	if Global.EQoppened == true:
+		open()
+	else:
+		close()
+
+func open():
+	self.visible = true
+	is_open = true
+
+func close():
+	visible = false
+	is_open = false
